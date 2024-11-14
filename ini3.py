fc3cc25893299bd854c8f0c08640f35e28e1f65b
alara <alarasbayer@gmail.com>
2024-11-14 16:49:22 +0100

    fix format issues

--- ini3.py
+++ ini3.py
@@ -3,10 +3,12 @@
 # (with space in between), inclusively. In other words, we should 
 # include elements s[b] and s[d]in our slice.
 
-s = "E54aE7M1xoW72UU6hpsDXwN8GAf91csliIKljswvhTRFrmA6ElX8EvswxfWrhZu6XinDznO1FCX1n94FMiddendorffinaiaHAdwOHkji9p0QPtQWaMJFLRZV3z2lritGPCVM677s4S8NsG6teSjGdderemensisdX4rMUSaenGAu42azfbCVA05B8LdG."
-a, b, c, d = 80, 95, 150, 159
-slice1=s[a:b+1]
-slice2=s[c:d+1]
-slice1 +" " + slice2
+{
+    s = "E54aE7M1xoW72UU6hpsDXwN8GAf91csliIKljswvhTRFrmA6ElX8EvswxfWrhZu6XinDznO1FCX1n94FMiddendorffinaiaHAdwOHkji9p0QPtQWaMJFLRZV3z2lritGPCVM677s4S8NsG6teSjGdderemensisdX4rMUSaenGAu42azfbCVA05B8LdG."
+    a, b, c, d = 80, 95, 150, 159
+    slice1=s[a:b+1]
+    slice2=s[c:d+1]
+    slice1 +" " + slice2
+}
 
 # 'Middendorffinaia deremensis'
