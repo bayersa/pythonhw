7f7090b746b8001bcfb14367dea0ff39853e3380
alara <alarasbayer@gmail.com>
2024-11-17 14:17:11 +0100

    rosalind HW problem 6

---
+++ ini6.py
@@ -0,0 +1,19 @@
+# Input string
+s = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom let it be And in my hour of darkness she is standing right in front of me Speaking words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the broken hearted people living in the world agree There will be an answer let it be For though they may be parted there is still a chance that they will see There will be an answer let it be Let it be let it be let it be let it be There will be an answer let it be Let it be let it be let it be let it be Whisper words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the night is cloudy there is still a light that shines on me Shine until tomorrow let it be I wake up to the sound of music Mother Mary comes to me Speaking words of wisdom let it be Let it be let it be let it be yeah let it be There will be an answer let it be Let it be let it be let it be yeah let it be Whisper words of wisdom let it be"
+
+# Split the string into words
+words = s.split()
+
+# Count occurrences of each word
+word_count = {}
+for word in words:
+    word_count[word] = word_count.get(word, 0) + 1
+
+# Print each word with its count
+for word, count in word_count.items():
+    print(word, count)
+
+
+
+
+    
