a9fa26dd9a72c2f416098ab87ad685126c53ee48
alara <alarasbayer@gmail.com>
2024-11-19 14:19:38 +0100

    change file name

---
+++ REVC.py
@@ -0,0 +1,16 @@
+DNA= "AAAACCCGGT"
+
+def complement(x):
+    if x=="A":
+        return "T"
+    elif x == "C":
+        return "G"
+    elif x == "G":
+        return "C"
+    elif x== "T":
+        return "A"
+    
+complement("A")
+
+#for base in DNA
+
---
+++ RNA.py
@@ -0,0 +1,9 @@
+DNA= "GATGGAACTTGACTACGTAAATT"
+RNA= ''
+
+for base in DNA:
+     if base == "T"
+        return "U"
+    else: 
+        return "base"
+print(RNA)
