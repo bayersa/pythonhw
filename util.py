419f803c03554e42e7a80cfd8afafb6fe85765cf
alara <alarasbayer@gmail.com>
2024-11-21 17:48:33 +0100

    function to reuse files

---
+++ util.py
@@ -0,0 +1,8 @@
+# a library of utility functions for rosalind exercises 
+def read_input(filepath): 
+	with open(filepath, 'r') as infile:
+		lines = infile.readlines()
+		stripped =[]
+		for line in lines:
+			stripped.append(line.strip())
+	return stripped
